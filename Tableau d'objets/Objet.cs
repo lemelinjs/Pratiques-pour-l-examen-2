69d832ea3a0294b3ee407779907fe0257721825d
lemelinjs <lemelin.jean-sebastien@carrefour.cegepvicto.ca>
2017-04-06 09:41:38 -0400

    Le tableau dimension2D envoie les même dimensions?

---
+++ Tableau d'objets/Objet.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tableau_d_objets
+{
+    public class Objet
+    {
+        public Grosseur[,] dimension2D;
+        private Random nombreAleatoire = new Random();
+
+        public enum Grosseur
+        {
+            Petite,
+            Moyenne,
+            Grosse,
+            TresGrosse
+        }
+
+        public void Initialiser()
+        {
+            dimension2D = new Grosseur[5, 2];
+
+            for (int j = 0; j < dimension2D.GetLength(1); j++)
+            {
+                for (int i = 0; i < dimension2D.GetLength(0); i++)
+                {
+                    int nombre = nombreAleatoire.Next(1, 5);
+                    switch (nombre)
+                    {
+                        case 1:
+                            dimension2D[i, j] = Grosseur.Petite;
+                            break;
+                        case 2:
+                            dimension2D[i, j] = Grosseur.Moyenne;
+                            break;
+                        case 3:
+                            dimension2D[i, j] = Grosseur.Grosse;
+                            break;
+                        case 4:
+                            dimension2D[i, j] = Grosseur.TresGrosse;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Tableau d'objets/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tableau_d_objets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Objet[] tab1 = new Objet[5];
+
+            for (int i = 0; i < tab1.Length; i++)
+            {
+                tab1[i] = new Objet();
+                tab1[i].Initialiser();
+            }
+
+            for (int k = 0; k < tab1.Length; k++)
+            {
+                //Afficher le tableau 2D appelé dimension2D qui est associé à l'espace k du tableau tab1
+                Console.WriteLine();
+                Console.WriteLine("Affichage du Tableau {0}", k+1);
+                for (int j = 0; j < tab1[k].dimension2D.GetLength(1); j++)
+                {
+                    for (int i = 0; i < tab1[k].dimension2D.GetLength(0); i++)
+                    {
+                        Console.Write("{0,-10} ", tab1[4].dimension2D[i, j]);
+                    }
+                    Console.WriteLine();
+                }
+            }
+            
+            
+            Console.ReadKey();
+        }
+    }
+}
